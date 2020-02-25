95277707ccbf7ff54a6cdf02e37befe0adcdcd4c
good <0419@qq.com>
2020-02-25 17:27:35 +0800

    draft done administrator api

--- jcart-administration-back/src/main/java/io/zkw/jcartadministrationback/controller/AdministratorController.java
+++ jcart-administration-back/src/main/java/io/zkw/jcartadministrationback/controller/AdministratorController.java
@@ -1,10 +1,9 @@
 package io.zkw.jcartadministrationback.controller;
 
-import io.zkw.jcartadministrationback.dto.in.AdministratorLoginInDTO;
-import io.zkw.jcartadministrationback.dto.in.AdministratorRestPwdDTO;
-import io.zkw.jcartadministrationback.dto.in.AdministratorUpdateProfileInDTO;
+import io.zkw.jcartadministrationback.dto.in.*;
 import io.zkw.jcartadministrationback.dto.out.AdministratorGetProfileOutDTO;
 import io.zkw.jcartadministrationback.dto.out.AdministratorListOutDTO;
+import io.zkw.jcartadministrationback.dto.out.AdministratorShowOutDTO;
 import io.zkw.jcartadministrationback.dto.out.PageOutDTO;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,30 +15,36 @@ public class AdministratorController {
     public String login(AdministratorLoginInDTO administratorLoginInDTO){
         return null;
     }
-
     @GetMapping("/getprofile")
     public AdministratorGetProfileOutDTO getProfile(@RequestParam(required = false) Integer adminstratorId){
     return null;
-
     }
-
     @PostMapping("/updateProdfile")
     public void updateProdfile(@RequestBody AdministratorUpdateProfileInDTO administratorUpdateProfileInDTO){
 
     }
-
     @GetMapping("/getPwdRestCode")
     public String getPwdRestCode(@RequestParam String email){
         return null;
     }
     @PostMapping("/resetPwd")
     public void resetPwd(@RequestBody AdministratorRestPwdDTO administratorRestPwdDTO){
-
     }
-
     @GetMapping("/getList")
     public PageOutDTO<AdministratorListOutDTO> getList(@RequestParam Integer pageNum){
 
         return null;
+    }
+    @GetMapping("/getById")
+    public AdministratorShowOutDTO getById(@RequestParam Integer administratorId){
+        return null;
+    }
+    @PostMapping("/creat")
+    public Integer create(@RequestBody AdministratorCreateInDTO administratorCreateInDTO){
+        return null;
+    }
+    @PostMapping("/update")
+    public void update(@RequestBody AdministratorUpdateInDTO administratorUpdateInDTO){
+        
     }
 }
---
+++ jcart-administration-back/src/main/java/io/zkw/jcartadministrationback/dto/in/AdministratorCreateInDTO.java
@@ -0,0 +1,50 @@
+package io.zkw.jcartadministrationback.dto.in;
+
+public class AdministratorCreateInDTO {
+
+    private String username;
+    private String password;
+    private String realName;
+    private String email;
+    private Byte status;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+}
