33c2f3b99bca5e6eca700bc606b2a961b118f4ae
good <0419@qq.com>
2020-02-25 18:50:58 +0800

    draft customer apis

---
+++ jcart-administration-back/src/main/java/io/zkw/jcartadministrationback/controller/CustomerController.java
@@ -0,0 +1,26 @@
+package io.zkw.jcartadministrationback.controller;
+
+import io.zkw.jcartadministrationback.dto.in.CustomerSearchInDTO;
+import io.zkw.jcartadministrationback.dto.out.CustomerListOutDTO;
+import io.zkw.jcartadministrationback.dto.out.CustomerShowOutDTO;
+import io.zkw.jcartadministrationback.dto.out.PageOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+
+    @GetMapping("/search")
+    public PageOutDTO<CustomerListOutDTO> search(CustomerSearchInDTO customerSearchInDTo, @RequestParam Integer pageNum){
+        return null;
+    }
+    @GetMapping("/getById")
+    public CustomerShowOutDTO getById(@RequestParam Integer customerId){
+        return null;
+    }
+    @PostMapping("/disable")
+    public void disable(@RequestParam Integer customerId){
+
+    }
+}
---
+++ jcart-administration-back/src/main/java/io/zkw/jcartadministrationback/dto/out/CustomerListOutDTO.java
@@ -0,0 +1,68 @@
+package io.zkw.jcartadministrationback.dto.out;
+
+public class CustomerListOutDTO {
+
+    private Integer customerId;
+    private String username;
+    private String realName;
+    private String email;
+    private String mobile;
+    private Byte status;
+    private Long createTimestamp;
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public Long getCreateTimestamp() {
+        return createTimestamp;
+    }
+
+    public void setCreateTimestamp(Long createTimestamp) {
+        this.createTimestamp = createTimestamp;
+    }
+}
