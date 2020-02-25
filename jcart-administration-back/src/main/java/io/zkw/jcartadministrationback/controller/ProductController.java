d59ca8483297dd4dbdddc1703ef9250421218676
good <0419@qq.com>
2020-02-25 18:07:32 +0800

    add product apis

--- jcart-administration-back/src/main/java/io/zkw/jcartadministrationback/controller/ProductController.java
+++ jcart-administration-back/src/main/java/io/zkw/jcartadministrationback/controller/ProductController.java
@@ -1,12 +1,12 @@
 package io.zkw.jcartadministrationback.controller;
 
+import io.zkw.jcartadministrationback.dto.in.ProductCreateInDTO;
 import io.zkw.jcartadministrationback.dto.in.ProductSearchInDTO;
+import io.zkw.jcartadministrationback.dto.in.ProductUpdateInDTO;
 import io.zkw.jcartadministrationback.dto.out.PageOutDTO;
 import io.zkw.jcartadministrationback.dto.out.ProductListOutDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import io.zkw.jcartadministrationback.dto.out.ProductShowOutDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -17,4 +17,19 @@ public class ProductController {
     public PageOutDTO<ProductListOutDTO> search(ProductSearchInDTO productSearchInDTO,@RequestParam Integer pageNum){
         return null;
     }
+
+    @GetMapping("/getById")
+    public ProductShowOutDTO getById(@RequestParam Integer productId){
+        return null;
+    }
+
+    @PostMapping("/create")
+    public Integer create(@RequestBody ProductCreateInDTO productCreateInDTO){
+        return null;
+    }
+    @PostMapping("/update")
+    public void update(@RequestBody ProductUpdateInDTO productUpdateInDTO){
+
+    }
+
 }
