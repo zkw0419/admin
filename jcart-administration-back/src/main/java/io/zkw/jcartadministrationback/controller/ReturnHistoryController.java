70cc52420b34a092af794d1549c2f47d20c368c1
good <0419@qq.com>
2020-02-26 16:57:16 +0800

    add administration return history apis

---
+++ jcart-administration-back/src/main/java/io/zkw/jcartadministrationback/controller/ReturnHistoryController.java
@@ -0,0 +1,24 @@
+package io.zkw.jcartadministrationback.controller;
+
+import io.zkw.jcartadministrationback.dto.in.ReturnHistoryCreateInDTO;
+import io.zkw.jcartadministrationback.dto.out.ReturnHistoryListOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/returnhistory")
+public class ReturnHistoryController {
+
+    @GetMapping("/getListByReturnId")
+
+    public List<ReturnHistoryListOutDTO> getListByReturnId(@RequestParam Integer returnId){
+        return null;
+    }
+
+    @PostMapping("/create")
+    public Integer create(@RequestBody ReturnHistoryCreateInDTO returnHistoryCreateInDTO){
+        return null;
+    }
+
+}
