24786f7c4b27e958abee5506919c88ea83beabe2
good <0419@qq.com>
2020-02-26 16:47:18 +0800

    add administration return apis

---
+++ jcart-administration-back/src/main/java/io/zkw/jcartadministrationback/controller/ReturnController.java
@@ -0,0 +1,26 @@
+package io.zkw.jcartadministrationback.controller;
+
+import io.zkw.jcartadministrationback.dto.in.ReturnSearchInDTO;
+import io.zkw.jcartadministrationback.dto.in.ReturnUpdateActionInDTO;
+import io.zkw.jcartadministrationback.dto.out.PageOutDTO;
+import io.zkw.jcartadministrationback.dto.out.ReturnListOutDTO;
+import io.zkw.jcartadministrationback.dto.out.ReturnShowOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/return")
+public class ReturnController {
+    @GetMapping("/search")
+    public PageOutDTO<ReturnListOutDTO> search(ReturnSearchInDTO returnSearchInDTO, @RequestParam Integer pageNUm){
+            return null;
+    }
+    @GetMapping("/getById")
+    public ReturnShowOutDTO getById(@RequestParam Integer returnId){
+        return null;
+    }
+
+    @PostMapping("/updateAction")
+    public void updateAction(@RequestBody ReturnUpdateActionInDTO returnUpdateActionInDTO){
+
+    }
+}
